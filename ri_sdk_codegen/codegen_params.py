a3853eaba0d4c26e1b0ab9b5c7b5ebf26e1ecb22
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-03-03 17:55:23 +0300

    create new codegen params

--- ri_sdk_codegen/codegen_params.py
+++ ri_sdk_codegen/codegen_params.py
@@ -2,20 +2,40 @@ import argparse
 from dataclasses import dataclass
 from pathlib import Path
 
+from .config import TYPES_MODULE_NAME
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 RI_SDK_CODEGEN_DIR = BASE_DIR / ".ri_sdk_codegen"
-RI_SDK_PYTHON_CODE_SRC_DIR = BASE_DIR / "src"
+RI_SDK_PYTHON_CODE_SRC_DIR = BASE_DIR / "ri_sdk"
+RI_SDK_PYTHON_TYPES_DIR = RI_SDK_PYTHON_CODE_SRC_DIR / TYPES_MODULE_NAME
 
-ALL_RI_SDK_PAGES_FILE_NAME = "ri_sdk_pages.txt"
-RI_SDK_PAGES_FILE_PATH = RI_SDK_CODEGEN_DIR / ALL_RI_SDK_PAGES_FILE_NAME
+ALL_RI_SDK_PAGES_FILENAME = "ri_sdk_pages.txt"
+RI_SDK_PAGES_FILEPATH = RI_SDK_CODEGEN_DIR / ALL_RI_SDK_PAGES_FILENAME
 URL_BASE = "https://docs.robointellect.ru"
 
 ROBOINTELLECT_BASE_SDK_FILENAME = "robointellect_base_sdk.py"
 ROBOINTELLECT_SDK_OUTPUT_FILEPATH = Path(
     RI_SDK_PYTHON_CODE_SRC_DIR / ROBOINTELLECT_BASE_SDK_FILENAME,
 )
-MAKO_TEMPLATE_FILENAME = "template.robointellect_base_sdk.py.mako"
-MAKO_TEMPLATE_FILEPATH = BASE_DIR / "ri_sdk_codegen" / MAKO_TEMPLATE_FILENAME
+MAKO_TEMPLATE_RI_SDK_FILENAME = "ri_sdk_base_wrapper.py.template.mako"
+MAKO_TEMPLATE_RI_SDK_METHOD_RETURN_TYPE_FILENAME = (
+    "ri_sdk_method_return_type.py.template.mako"
+)
+MAKO_TEMPLATE_RI_SDK_METHOD_RETURN_TYPES_INIT_FILENAME = (
+    "ri_sdk_types_init.py.template.mako"
+)
+TEMPLATE_FOLDER_NAME = "templates"
+TEMPLATE_FOLDER_DIR = BASE_DIR / "ri_sdk_codegen" / TEMPLATE_FOLDER_NAME
+MAKO_TEMPLATE_RI_SDK_FILEPATH = (
+    TEMPLATE_FOLDER_DIR / MAKO_TEMPLATE_RI_SDK_FILENAME
+)
+MAKO_TEMPLATE_RI_SDK_METHOD_RETURN_TYPE_FILEPATH = (
+    TEMPLATE_FOLDER_DIR / MAKO_TEMPLATE_RI_SDK_METHOD_RETURN_TYPE_FILENAME
+)
+MAKO_TEMPLATE_RI_SDK_METHOD_RETURN_TYPES_INIT_FILEPATH = (
+    TEMPLATE_FOLDER_DIR
+    / MAKO_TEMPLATE_RI_SDK_METHOD_RETURN_TYPES_INIT_FILENAME
+)
 DOCS_ENTRY_PATH = "/docs/category/ri-sdk-api"
 
 parser = argparse.ArgumentParser(
@@ -64,8 +84,8 @@ parser.add_argument(
 )
 parser.add_argument(
     "--pages-urls-filepath",
-    default=RI_SDK_PAGES_FILE_PATH,
-    help=f"Where to store known urls (default: {RI_SDK_PAGES_FILE_PATH})",
+    default=RI_SDK_PAGES_FILEPATH,
+    help=f"Where to store known urls (default: {RI_SDK_PAGES_FILEPATH})",
     type=Path,
 )
 parser.add_argument(
@@ -73,13 +93,6 @@ parser.add_argument(
     default=ROBOINTELLECT_SDK_OUTPUT_FILEPATH,
     help="File path for the output file script "
     f"(default: {ROBOINTELLECT_SDK_OUTPUT_FILEPATH})",
-    type=Path,
-)
-parser.add_argument(
-    "--sdk-template-filepath",
-    default=MAKO_TEMPLATE_FILEPATH,
-    help="File path for the Mako template "
-    f"(default: {MAKO_TEMPLATE_FILEPATH})",
     type=Path,
 )
 
@@ -91,13 +104,21 @@ class CodegenParams:
     generate_sdk: bool = False
     verbose: bool = False
     sort_methods: bool = True
+    remove_existing_types: bool = True
     base_url: str = URL_BASE
     docs_entry_path: str = DOCS_ENTRY_PATH
-    ri_sdk_pages_file_path: Path = RI_SDK_PAGES_FILE_PATH
+    ri_sdk_pages_file_path: Path = RI_SDK_PAGES_FILEPATH
     output_py_script: Path = ROBOINTELLECT_SDK_OUTPUT_FILEPATH
-    sdk_template_filepath: Path = MAKO_TEMPLATE_FILEPATH
+    sdk_template_filepath: Path = MAKO_TEMPLATE_RI_SDK_FILEPATH
+    method_return_type_template_filepath: Path = (
+        MAKO_TEMPLATE_RI_SDK_METHOD_RETURN_TYPE_FILEPATH
+    )
+    method_return_types_init_filepath: Path = (
+        MAKO_TEMPLATE_RI_SDK_METHOD_RETURN_TYPES_INIT_FILEPATH
+    )
     ri_sdk_codegen_dir: Path = RI_SDK_CODEGEN_DIR
     ri_sdk_python_code_src_dir: Path = RI_SDK_PYTHON_CODE_SRC_DIR
+    ri_sdk_python_types_dir: Path = RI_SDK_PYTHON_TYPES_DIR
 
     @property
     def docs_crawl_start_url(self) -> str:
@@ -115,5 +136,4 @@ def get_params() -> CodegenParams:
         base_url=args.base_url,
         ri_sdk_pages_file_path=args.pages_urls_filepath,
         output_py_script=args.output_py_script,
-        sdk_template_filepath=args.sdk_template_filepath,
     )
--- ri_sdk_codegen/config.py
+++ ri_sdk_codegen/config.py
@@ -3,3 +3,4 @@ LOG_FORMAT = (
     "[%(asctime)s.%(msecs)03d] "
     "%(module)s:%(lineno)d %(levelname)s - %(message)s"
 )
+TYPES_MODULE_NAME = "types"
