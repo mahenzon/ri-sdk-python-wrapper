649428e29046d1d5faa7a3c5de6908304267a411
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-03-06 11:38:37 +0300

    import versions vars to ri_sdk init

--- ri_sdk/__init__.py
+++ ri_sdk/__init__.py
@@ -1,4 +1,6 @@
 __all__ = (
+    "__version__",
+    "__sdk_version__",
     "RoboIntellectSDK",
     "types",
     "utils",
@@ -6,4 +8,5 @@ __all__ = (
 )
 
 from ri_sdk import exceptions, types, utils
+from ri_sdk.__about__ import __sdk_version__, __version__
 from ri_sdk.sdk_wrapper import RoboIntellectSDK
