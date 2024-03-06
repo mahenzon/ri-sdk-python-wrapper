09c946b8ffedbdbcba7dc3ed9dec1f2a86e09519
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-03-06 09:38:35 +0300

    add auto_len field to param option

--- ri_sdk_codegen/types/options_override.py
+++ ri_sdk_codegen/types/options_override.py
@@ -1,10 +1,15 @@
 from typing import Literal
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class ParamOptions(BaseModel):
     direction: Literal["input", "output"] | None = None
+    auto_len: str | None = Field(
+        default=None,
+        description="If set, find var named after this value "
+        "and replace it with automatically calculated value based on the param name",
+    )
 
 
 class MethodOptions(BaseModel):
