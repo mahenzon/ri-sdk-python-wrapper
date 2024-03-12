901138480d4fe66c48232d48ed27c8083f1202f1
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-03-12 09:20:06 +0300

    create loggers module

--- ri_sdk/__init__.py
+++ ri_sdk/__init__.py
@@ -5,8 +5,9 @@ __all__ = (
     "types",
     "utils",
     "exceptions",
+    "loggers",
 )
 
-from ri_sdk import exceptions, types, utils
+from ri_sdk import exceptions, loggers, types, utils
 from ri_sdk.__about__ import __sdk_version__, __version__
 from ri_sdk.sdk_wrapper import RoboIntellectSDK
---
+++ ri_sdk/loggers.py
@@ -0,0 +1,4 @@
+import logging
+
+wrapper = logging.getLogger("risdk.wrapper")
+utils = logging.getLogger("risdk.utils")
