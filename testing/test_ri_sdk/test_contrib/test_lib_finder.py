3b9038b69b82131ff02eca1480287c406ab0219f
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-03-24 14:38:01 +0300

    refactor lib_finder utility, add tests coverage

--- testing/test_ri_sdk/test_contrib/test_lib_finder.py
+++ testing/test_ri_sdk/test_contrib/test_lib_finder.py
@@ -1,6 +1,10 @@
+from ctypes import cdll
+from pathlib import Path
+
 import pytest
 
 from ri_sdk.contrib import get_lib
+from ri_sdk.contrib.lib_finder import get_lib_path, validate_lib_filepath
 
 
 @pytest.fixture()
@@ -8,6 +12,11 @@ def lib_filepath(tmpdir):
     path = tmpdir.mkdir("libpath").join("lib.so")
     path.write("")
     return path
+
+
+@pytest.fixture()
+def mocked_load_library(mocker):
+    return mocker.patch.object(cdll, "LoadLibrary", autospec=True)
 
 
 class TestGetLib:
@@ -23,8 +32,63 @@ class TestGetLib:
         ):
             get_lib()
 
-    def test_get_predefined_lib(self, lib_filepath, mocker):
-        mock_cdll = mocker.patch("ri_sdk.contrib.lib_finder.cdll", autospec=True)
+    def test_get_predefined_lib(self, lib_filepath, mocked_load_library):
         res = get_lib(library_filepath=lib_filepath)
-        assert res is mock_cdll.LoadLibrary.return_value
-        mock_cdll.LoadLibrary.assert_called_once_with(str(lib_filepath))
+        assert res is mocked_load_library.return_value
+        mocked_load_library.assert_called_once_with(str(lib_filepath))
+
+    @pytest.mark.parametrize("platform_name", ["Linux", "Windows"])
+    def test_library_dir_defined(
+        self,
+        mocker,
+        tmpdir,
+        mocked_load_library,
+        platform_name,
+    ):
+        mocker.patch(
+            "ri_sdk.contrib.lib_finder.platform.system",
+            return_value=platform_name,
+        )
+        mock_validate_lib_filepath = mocker.patch(
+            "ri_sdk.contrib.lib_finder.validate_lib_filepath",
+        )
+        # Create a temporary directory for the library_dir
+        library_dir = tmpdir.mkdir("library_dir")
+
+        # Call the get_lib function with the library_dir parameter
+        res = get_lib(library_dir=str(library_dir))
+        assert res is mocked_load_library.return_value
+
+        mock_validate_lib_filepath.assert_called_once()
+
+
+class TestValidateLibFilepath:
+    def test_valid_lib_filepath_raises(self):
+        lib_filepath = Path("path/to/nonexistent_lib.so")
+        assert lib_filepath.is_file() is False
+
+        with pytest.raises(FileNotFoundError):
+            validate_lib_filepath(lib_filepath)
+
+
+class TestGetLibPath:
+    def test_lib_path_with_defined_env(self, mocker):
+        new_path = "some/path/to/binary"
+        mocker.patch("ri_sdk.contrib.lib_finder.LIB_RISDK_PATH", new=new_path)
+
+        library_dir = None
+        default_lib_dir_path = Path("/default/path")
+
+        result = get_lib_path(library_dir, default_lib_dir_path)
+
+        assert result == Path(new_path)
+
+    def test_lib_path_not_defined_in_env(self, mocker):
+        mocker.patch("ri_sdk.contrib.lib_finder.LIB_RISDK_PATH", new=None)
+
+        library_dir = None
+        default_lib_dir_path = Path("/default/path")
+
+        result = get_lib_path(library_dir, default_lib_dir_path)
+
+        assert result == default_lib_dir_path
